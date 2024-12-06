dcc5798f6624e67815620b7b00cf30701b166b48
alissonteles <alisson_teles_ribeio@outlook.com>
2024-12-06 07:53:32 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,11 @@
+class hero{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+}
+
+let heroi = new hero("Alisson", 34, "guerreiro")
+
+console.log(heroi)
